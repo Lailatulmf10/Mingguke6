557d75e6852be7d2e4c4031d696e3a0c3d9944b8
Lailatul <mufidalailatul10@gmail.com>
2021-11-17 17:20:49 +0700

    Autentikasii

--- database/seeders/UserBaruSeeder.php
+++ database/seeders/UserBaruSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
+class UserBaruSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,9 +15,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'administrator',
-            'email' => 'admin@admin.com',
+        db::table('users')->insert([
+            'username' => 'admin',
+            'name' => 'Administrator Baru',
+            'email' => 'admin.baru@admin.com',
             'password' => Hash::make('password'),
         ]);
     }
